70f2bfdc957284fd5725e9041d60fb9bcce49e31
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-12-03 15:18:06 +1300

    Updating the tests to verify that consul and unbound are correctly installed
    references #4

--- tests/integration/pester/Firewall.Tests.ps1
+++ tests/integration/pester/Firewall.Tests.ps1
@@ -5,15 +5,57 @@ Describe 'The firewall' {
         It 'should return a status' {
             $ufwOutput | Should Not Be $null
             $ufwOutput.GetType().FullName | Should Be 'System.Object[]'
-            $ufwOutput.Length | Should Be 7
+            $ufwOutput.Length | Should Be 19
         }
 
         It 'should be enabled' {
             $ufwOutput[0] | Should Be 'Status: active'
         }
+    }
 
-        It 'should allow SSH' {
-            ($ufwOutput | Where-Object {$_ -match '(22)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be ''
+    Context 'should allow SSH' {
+        It 'on port 22' {
+            ($ufwOutput | Where-Object {$_ -match '(22/tcp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+    }
+
+    Context 'should allow consul' {
+        It 'on port 8300' {
+            ($ufwOutput | Where-Object {$_ -match '(8300/tcp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+
+        It 'on TCP port 8301' {
+            ($ufwOutput | Where-Object {$_ -match '(8301/tcp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+
+        It 'on UDP port 8301' {
+            ($ufwOutput | Where-Object {$_ -match '(8301/udp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+
+        It 'on TCP port 8302' {
+            ($ufwOutput | Where-Object {$_ -match '(8302/tcp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+
+        It 'on UDP port 8302' {
+            ($ufwOutput | Where-Object {$_ -match '(8302/udp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+
+        It 'on port 8500' {
+            ($ufwOutput | Where-Object {$_ -match '(8500/tcp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+
+        It 'on UDP port 8600' {
+            ($ufwOutput | Where-Object {$_ -match '(8600/udp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+    }
+
+    Context 'should allow unbound' {
+        It 'on TCP port 53' {
+            ($ufwOutput | Where-Object {$_ -match '(53/tcp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
+        }
+
+        It 'on UDP port 53' {
+            ($ufwOutput | Where-Object {$_ -match '(53/udp)\s*(ALLOW)\s*(Anywhere)'} ) | Should Not Be $null
         }
     }
 }
