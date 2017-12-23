c3f9f23ccadcc11af40baa6c8c64f8cd05d707f4
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-12-23 23:57:53 +1300

    Updating how provisioning works so that we can reuse the functions in a derived image

--- src/cookbooks/base_linux/recipes/provisioning.rb
+++ src/cookbooks/base_linux/recipes/provisioning.rb
@@ -27,8 +27,8 @@ end
 # CONFIGURE THE PROVISIONING SCRIPT
 #
 
-# Create the provisioning script
-file '/etc/init.d/provision.sh' do
+# Create the script containing the helper functions
+file '/etc/init.d/provision_helpers.sh' do
   action :create
   content <<~BASH
     #!/bin/bash
@@ -41,6 +41,98 @@ file '/etc/init.d/provision.sh' do
             [ "${_ip#127.0.0.1}" ] && echo $_ip && return 0
         done< <(LANG=C /sbin/ifconfig eth0)
     }
+
+    function setHostName {
+      #Assign existing hostname to $hostn
+      hostn=$(cat /etc/hostname)
+
+      # Generate a 16 character password
+      POSTFIX=$(pwgen --no-capitalize 16 1)
+
+      NAME="cv${RESOURCE_SHORT_NAME}-${RESOURCE_VERSION_MAJOR}-${RESOURCE_VERSION_MINOR}-${RESOURCE_VERSION_PATCH}-${POSTFIX}"
+      sudo sed -i "s/$hostn/$NAME/g" /etc/hosts
+      sudo sed -i "s/$hostn/$NAME/g" /etc/hostname
+    }
+  BASH
+  mode '755'
+end
+
+file '/etc/init.d/provision_consul.sh' do
+  action :create
+  content <<~BASH
+    #!/bin/bash
+
+    function f_provisionConsul {
+      sudo systemctl stop consul.service
+      sudo rm -rfv /var/lib/consul/*
+
+      cp -a /mnt/dvd/consul/consul_region.json /etc/consul/conf.d/region.json
+      dos2unix /etc/consul/conf.d/region.json
+
+      cp -a /mnt/dvd/consul/consul_secrets.json /etc/consul/conf.d/secrets.json
+      dos2unix /etc/consul/conf.d/secrets.json
+
+      # Copy the consul client files if they exist
+      if [ ! -f /mnt/dvd/consul/client/consul_client_location.json ]; then
+        cp -a /mnt/dvd/consul/client/consul_client_location.json /etc/consul/conf.d/location.json
+        dos2unix /etc/consul/conf.d/location.json
+      fi
+
+      # Copy the consul server files if they exist
+      if [ ! -f /mnt/dvd/consul/server/consul_server_bootstrap.json ]; then
+        cp -a /mnt/dvd/consul/server/consul_server_bootstrap.json /etc/consul/conf.d/bootstrap.json
+        dos2unix /etc/consul/conf.d/bootstrap.json
+      fi
+
+      if [ ! -f /mnt/dvd/consul/server/consul_server_location.json ]; then
+        cp -a /mnt/dvd/consul/server/consul_server_location.json /etc/consul/conf.d/location.json
+        dos2unix /etc/consul/conf.d/location.json
+      fi
+    }
+  BASH
+  mode '755'
+end
+
+file '/etc/init.d/provision_consul-template.sh' do
+  action :create
+  content <<~BASH
+    #!/bin/bash
+
+    function f_provisionConsulTemplate {
+      cp -a /mnt/dvd/consul-template/vault.hcl /etc/consul-template.d/conf/vault.hcl
+      dos2unix /etc/consul-template.d/conf/vault.hcl
+
+      sudo systemctl enable consul-template.service
+    }
+  BASH
+  mode '755'
+end
+
+file '/etc/init.d/provision_unbound.sh' do
+  action :create
+  content <<~BASH
+    #!/bin/bash
+
+    function f_provisionUnbound {
+      cp -a /mnt/dvd/unbound/unbound_zones.conf /etc/unbound.d/unbound_zones.conf
+      dos2unix /etc/unbound.d/unbound_zones.conf
+
+      sudo systemctl enable unbound.service
+    }
+  BASH
+  mode '755'
+end
+
+# Create the provisioning script
+file '/etc/init.d/provision.sh' do
+  action :create
+  content <<~BASH
+    #!/bin/bash
+
+    . /etc/init.d/provision_helpers.sh
+    . /etc/init.d/provision_consul.sh
+    . /etc/init.d/provision_consul-template.sh
+    . /etc/init.d/provision_unbound.sh
 
     FLAG="/var/log/firstboot.log"
     if [ ! -f $FLAG ]; then
@@ -78,55 +170,23 @@ file '/etc/init.d/provision.sh' do
       # CONSUL CONFIGURATION
       #
       # Stop the consul service and kill the data directory. It will have the consul node-id in it which must go!
-      sudo systemctl stop consul.service
-      sudo rm -rfv /var/lib/consul/*
-
-      cp -a /mnt/dvd/consul/consul_region.json /etc/consul/conf.d/region.json
-      dos2unix /etc/consul/conf.d/region.json
-
-      cp -a /mnt/dvd/consul/consul_secrets.json /etc/consul/conf.d/secrets.json
-      dos2unix /etc/consul/conf.d/secrets.json
-
-      # Copy the consul client files if they exist
-      if [ ! -f /mnt/dvd/consul/client/consul_client_location.json ]; then
-        cp -a /mnt/dvd/consul/client/consul_client_location.json /etc/consul/conf.d/location.json
-        dos2unix /etc/consul/conf.d/location.json
-      fi
-
-      # Copy the consul server files if they exist
-      if [ ! -f /mnt/dvd/consul/server/consul_server_bootstrap.json ]; then
-        cp -a /mnt/dvd/consul/server/consul_server_bootstrap.json /etc/consul/conf.d/bootstrap.json
-        dos2unix /etc/consul/conf.d/bootstrap.json
-      fi
-
-      if [ ! -f /mnt/dvd/consul/server/consul_server_location.json ]; then
-        cp -a /mnt/dvd/consul/server/consul_server_location.json /etc/consul/conf.d/location.json
-        dos2unix /etc/consul/conf.d/location.json
-      fi
+      f_provisionConsul
 
       #
-      # CONSUL CONFIGURATION
+      # CONSUL-TEMPLATE CONFIGURATION
       #
-      cp -a /mnt/dvd/consul-template/vault.hcl /etc/consul-template.d/conf/vault.hcl
-      dos2unix /etc/consul-template.d/conf/vault.hcl
+      f_provisionConsulTemplate
 
       #
       # UNBOUND CONFIGURATION
       #
-      cp -a /mnt/dvd/unbound/unbound_zones.conf /etc/unbound.d/unbound_zones.conf
-      dos2unix /etc/unbound.d/unbound_zones.conf
+      f_provisionUnbound
 
       #
       # UNMOUNT DVD
       #
       umount /dev/dvd
       eject -T /dev/dvd
-
-      #
-      # ENABLE SERVICES
-      #
-      sudo systemctl enable unbound.service
-      sudo systemctl enable consul-template.service
 
       # The next line creates an empty file so it won't run the next boot
       touch $FLAG
@@ -153,6 +213,7 @@ file '/etc/systemd/system/provision.service' do
     Type=oneshot
     ExecStart=/etc/init.d/provision.sh
     RemainAfterExit=true
+    EnvironmentFile=-/etc/environment
 
     [Install]
     WantedBy=network-online.target
