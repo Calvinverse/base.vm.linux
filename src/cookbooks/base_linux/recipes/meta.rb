2b00698eee356469b2d4a9168620c301ba84055c
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-01-29 21:57:19 +1300

    Write the consul version for the current image to the environment variables so that the test scripts can use the environment variable instead of hard-coding it

--- src/cookbooks/base_linux/recipes/meta.rb
+++ src/cookbooks/base_linux/recipes/meta.rb
@@ -8,10 +8,12 @@
 #
 
 resource_name = node['resource']['name']
-ruby_block 'set_base_image_product_name' do
+consul_version = node['consul']['version']
+ruby_block 'set_base_image_environment_information' do
   block do
     file = Chef::Util::FileEdit.new('/etc/environment')
     file.insert_line_if_no_match("BASE_IMAGE=#{resource_name}", "BASE_IMAGE=#{resource_name}")
+    file.insert_line_if_no_match("CONSUL_VERSION=#{consul_version}", "CONSUL_VERSION=#{consul_version}")
     file.write_file
   end
 end
