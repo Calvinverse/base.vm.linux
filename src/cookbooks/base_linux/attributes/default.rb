20440fb5fa3c0598a13b21fa8e01669104d6e2c6
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-03-06 08:43:41 +1300

    Removing unused attributes

--- src/cookbooks/base_linux/attributes/default.rb
+++ src/cookbooks/base_linux/attributes/default.rb
@@ -62,7 +62,6 @@ default['consul_template']['config_path'] = '/etc/consul-template.d/conf'
 default['consul_template']['template_path'] = '/etc/consul-template.d/templates'
 
 default['consul_template']['vault_template_file'] = 'consul_template_vault.ctmpl'
-default['consul_template']['vault_token_template_file'] = 'consul_template_vault_token.ctmpl'
 default['consul_template']['script_config_file'] = '/tmp/consul_template_vault.sh'
 
 default['consul_template']['service_user'] = 'consul_template'
