78132e4dac1820a5fd332a1d19be5d46b9d56601
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-05-14 09:08:23 +1200

    Update to consul 1.1.0

--- src/cookbooks/base_linux/attributes/default.rb
+++ src/cookbooks/base_linux/attributes/default.rb
@@ -4,7 +4,7 @@
 # CONSUL
 #
 
-default['consul']['version'] = '1.0.7'
+default['consul']['version'] = '1.1.0'
 default['consul']['config']['domain'] = 'consulverse'
 
 # This is not a consul server node
