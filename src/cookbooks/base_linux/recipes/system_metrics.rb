b3767628d67068250c2565eef6d90c46868c6f6f
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-08-04 23:23:39 +1200

    reload is not an option for systemctl, restart is however

--- src/cookbooks/base_linux/recipes/system_metrics.rb
+++ src/cookbooks/base_linux/recipes/system_metrics.rb
@@ -488,7 +488,7 @@ file "#{consul_template_config_path}/telegraf.hcl" do
       # command will only run if the resulting template changes. The command must
       # return within 30s (configurable), and it must have a successful exit code.
       # Consul Template is not a replacement for a process monitor or init system.
-      command = "chown #{node['telegraf']['service_user']}:#{node['telegraf']['service_group']} #{telegraf_config_file} && systemctl reload #{telegraf_service}"
+      command = "chown #{node['telegraf']['service_user']}:#{node['telegraf']['service_group']} #{telegraf_config_file} && systemctl restart #{telegraf_service}"
 
       # This is the maximum amount of time to wait for the optional command to
       # return. Default is 30s.
