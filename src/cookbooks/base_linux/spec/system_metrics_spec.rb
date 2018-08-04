df2d2dda3d1233bd2fbffbb28ca8a21268ffa789
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-08-05 11:22:22 +1200

    Fix the way consul-template restarts telegraf because it doesn't have a default shell so you have to specify one

--- src/cookbooks/base_linux/spec/system_metrics_spec.rb
+++ src/cookbooks/base_linux/spec/system_metrics_spec.rb
@@ -469,7 +469,7 @@ describe 'base_linux::system_metrics' do
         # command will only run if the resulting template changes. The command must
         # return within 30s (configurable), and it must have a successful exit code.
         # Consul Template is not a replacement for a process monitor or init system.
-        command = "chown telegraf:telegraf /etc/telegraf/telegraf.conf && systemctl restart telegraf"
+        command = "/bin/bash -c 'chown telegraf:telegraf /etc/telegraf/telegraf.conf && systemctl restart telegraf'"
 
         # This is the maximum amount of time to wait for the optional command to
         # return. Default is 30s.
