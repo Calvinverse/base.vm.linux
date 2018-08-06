d1dfa008ac3f86c9e9eb52c08230aa5997c9d8b1
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-08-06 21:18:18 +1200

    Updating the syslog-ng config file version

--- src/cookbooks/base_linux/recipes/system_logs.rb
+++ src/cookbooks/base_linux/recipes/system_logs.rb
@@ -44,7 +44,7 @@ syslog_ng_template_file = node['syslog_ng']['consul_template_file']
 file "#{consul_template_template_path}/#{syslog_ng_template_file}" do
   action :create
   content <<~CONF
-    @version: 3.13
+    @version: 3.16
 
     ########################
     # Destinations
