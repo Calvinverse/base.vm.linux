f36cfcaa96e2d6d1fbfdaa027c073bf85753968e
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-01-05 22:52:36 +1300

    Have to configure the ssh authentication after we configure consul-template because we need consul-template to setup the certificates
    references #41

--- src/cookbooks/base_linux/recipes/default.rb
+++ src/cookbooks/base_linux/recipes/default.rb
@@ -20,11 +20,12 @@ include_recipe 'base_linux::firewall'
 include_recipe 'base_linux::meta'
 
 include_recipe 'base_linux::certificates'
-include_recipe 'base_linux::authentication_ssh'
-# include_recipe 'base_linux::authentication_pki'
 
 include_recipe 'base_linux::consul'
 include_recipe 'base_linux::consul_template'
+
+include_recipe 'base_linux::authentication_ssh'
+# include_recipe 'base_linux::authentication_pki'
 
 include_recipe 'base_linux::system'
 include_recipe 'base_linux::system_logs'
